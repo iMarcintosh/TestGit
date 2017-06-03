347351250ffaf8796173712ac9831857da2fca4d
marc <loeffler.marc@gmail.com>
2017-06-03 02:44:59 +0200

    new comment.

--- app/src/main/java/com/marcintosh/testgit/MainActivity.java
+++ app/src/main/java/com/marcintosh/testgit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Test fghfhfhfhfghfghfgh
         System.out.println("GitHub");
+        // New Comment
     }
 }
